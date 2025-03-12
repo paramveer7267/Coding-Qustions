bc87ad315117833911f3b276cf96b6c13eb7f8c8
Paramveer Singh <114360831+paramveer7267@users.noreply.github.com>
2025-03-12 23:49:17 +0530

    Update 2529. Maximum Count of Positive Integer and Negative Integer.cpp

--- Easy/2529. Maximum Count of Positive Integer and Negative Integer.cpp
+++ Easy/2529. Maximum Count of Positive Integer and Negative Integer.cpp
@@ -12,24 +12,42 @@
 class Solution
 {
 public:
-  int maximumCount(vector<int> &nums)
-  {
+  int maximumCount(vector<int> &nums){
     int n = nums.size();
     int u = 0;
     int d = 0;
 
-    for (int i = 0; i < n; i++)
-    {
-      if (nums[i] < 0)
-      {
+    for (int i = 0; i < n; i++){
+      if (nums[i] < 0){
         d++;
       }
-      if (nums[i] > 0)
-      {
+      if (nums[i] > 0){
         u++;
       }
     }
-
+      
     return max(u, d);
   }
+};
+
+// Using STL/count_if Approach
+// Time Complexity : O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+
+        auto lambdaP = [](int num){
+            return num > 0;
+        };
+        auto lambdaN = [](int num){
+            return num < 0;
+        };
+
+        int pos = count_if(begin(nums),end(nums),lambdaP);
+        int neg = count_if(begin(nums),end(nums),lambdaN);
+
+        return max(pos,neg);
+    }
 };
