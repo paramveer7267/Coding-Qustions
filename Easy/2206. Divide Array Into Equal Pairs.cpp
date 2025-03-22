36f89b6cc9112f627b30d347854bee158343bb93
Paramveer Singh <114360831+paramveer7267@users.noreply.github.com>
2025-03-22 20:03:24 +0530

    Update 2206. Divide Array Into Equal Pairs.cpp

--- Easy/2206. Divide Array Into Equal Pairs.cpp
+++ Easy/2206. Divide Array Into Equal Pairs.cpp
@@ -9,7 +9,7 @@
 
 // Using Constraint Observation Approach
 // Time Complexity :O(N)
-// Space Complexity:O(1) or O(501)
+// Space Complexity:O(500) ~ O(1)
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
@@ -24,6 +24,92 @@ public:
                 return false;
             }
         }
+        return true;
+    }
+};
+
+
+
+// Using Map
+// Time Complexity :O(N)
+// Space Complexity:O(N)
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        for(int &x : nums) {
+            mp[x]++;
+        }
+
+        for(auto &it : mp) {
+            if(it.second % 2 != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
+// Using Set
+// Time Complexity :O(N)
+// Space Complexity:O(N)
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_set<int> st;
+
+        for(int &x : nums) {
+            if(st.count(x)) {
+                st.erase(x);
+            } else {
+                st.insert(x);
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+
+
+// Using sorting and comparing
+// Time Complexity :O(N*logN)
+// Space Complexity:O(1)
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        int n = nums.size();
+        sort(begin(nums), end(nums));
+
+        for(int i = 1; i < n; i+=2) {
+            if(nums[i] != nums[i-1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
+// Using sorting and xor
+// Time Complexity :O(N * logN)
+// Space Complexity:O(1)
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        int n = nums.size();
+        sort(begin(nums), end(nums));
+
+        for(int i = 1; i < n; i+=2) {
+            if((nums[i] ^ nums[i-1]) != 0) {
+                return false;
+            }
+        }
+
         return true;
     }
 };
