b1341f0cfa9d953c0fce0b2fdca35b237a4fbf95
Paramveer Singh <114360831+paramveer7267@users.noreply.github.com>
2025-03-25 23:38:32 +0530

    Update 88. Merge Sorted Array.cpp

--- Easy/88. Merge Sorted Array.cpp
+++ Easy/88. Merge Sorted Array.cpp
@@ -1,1 +1,52 @@
+/*
+    LeetCode Easy               : 88. Merge Sorted Array
+    Leetcode Link               : https://leetcode.com/problems/merge-sorted-array/description/
+*/
 
+/*********************************************************** C++ **************************************************/
+
+
+// Using Two Pointers
+// Time Complexity:O(n + m)
+// Space Complexity:O(1)
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = n+m-1;
+        for(int p =0;p<m+n;p++){
+            if(i<0 or j<0) break;
+            if(nums1[i]>=nums2[j]){
+                nums1[k--] = nums1[i--];
+
+            }
+            else{
+                nums1[k--] = nums2[j--];
+
+            }
+        
+       
+        }
+        while(j>=0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+};
+
+
+
+// Using Sorting
+// Time Complexity:O((m + n)logn)
+// Space Complexity:O(1)
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+          for (int j = 0, i = m; j<n; j++){
+            nums1[i] = nums2[j];
+            i++;
+        }
+        sort(nums1.begin(),nums1.end());
+    
+    }
+};
