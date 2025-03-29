f935d18e10c4d22850f4ea2a4872f2f806d59ed2
Paramveer Singh <114360831+paramveer7267@users.noreply.github.com>
2025-03-29 22:54:02 +0530

    Update 2818. Apply Operations to Maximize Score.cpp

--- Hard/2818. Apply Operations to Maximize Score.cpp
+++ Hard/2818. Apply Operations to Maximize Score.cpp
@@ -1,1 +1,12 @@
+/*
+    LeetCode Hard               : 1358. Number of Substrings Containing All Three Characters
+    LeetCode Link               : https://leetcode.com/problems/apply-operations-to-maximize-score/description
+*/
 
+/*********************************************************** C++ **************************************************/
+
+
+
+// Using Sliding Window Approach
+// Time Complexity : O(n)
+// Space Complexity: O(1)
