6b4d46ca5f5cb64240758fcbac547a12531e1a38
Paramveer Singh <114360831+paramveer7267@users.noreply.github.com>
2025-04-02 23:44:07 +0530

    Update 2873. Maximum Value of an Ordered Triplet I.cpp

--- Easy/2873. Maximum Value of an Ordered Triplet I.cpp
+++ Easy/2873. Maximum Value of an Ordered Triplet I.cpp
@@ -31,6 +31,50 @@ public:
 
 
 
-// Using AOC 
-// Time Complexity : O(n³)
+// Using Better Approach
+// Time Complexity : O(n)
+// Space Complexity: O(n)
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = size(nums);
+        long long ans = INT_MIN;
+        vector<int> leftMax(n);
+        vector<int> rightMax(n);
+
+        for (int j = 1; j < n; j++) {
+            leftMax[j] = max(leftMax[j - 1], nums[j - 1]);
+        }
+
+        for (int j = n - 2; j >= 0; j--) {
+            rightMax[j] = max(rightMax[j + 1], nums[j + 1]);
+        }
+
+        for(int j = 1 ;j<n;j++){
+            ans = max(ans,(long long)(leftMax[j] - nums[j]) * rightMax[j]);
+        }
+        return ans;
+    }
+};
+
+
+
+// Using Optimal Approach
+// Time Complexity : O(n)
 // Space Complexity: O(1)
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = size(nums);
+        long long ans = INT_MIN;
+        long long maxDiff = 0;
+        long long maxi = 0;
+
+        for(int  k = 0;k<n;k++){
+            ans = max(ans , maxDiff * nums[k]);
+            maxDiff = max(maxDiff , maxi - nums[k]);
+            maxi = max(maxi,(long long)nums[k]);
+        }
+        return ans;
+    }
+};
