25b82272566937e23f20fa84fd8ecc45d7eb4bfd
Paramveer Singh <114360831+paramveer7267@users.noreply.github.com>
2025-04-09 22:53:09 +0530

    Update 3396. Minimum Number of Operations to Make Elements in Array Distinct.cpp

--- Easy/3396. Minimum Number of Operations to Make Elements in Array Distinct.cpp
+++ Easy/3396. Minimum Number of Operations to Make Elements in Array Distinct.cpp
@@ -38,3 +38,21 @@ public:
 
 
 
+// Using Right pass
+// Time Complexity: O(n)
+// Space COmplexity:O(n)
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> st;
+        int ops = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (st.count(nums[i])) {
+                return ceil((i + 1)/3.0);
+            }
+            st.insert(nums[i]);
+        }
+        return 0;
+    }
+};
