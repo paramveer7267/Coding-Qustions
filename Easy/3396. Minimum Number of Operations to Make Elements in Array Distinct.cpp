bbfc8240485bb8f46e855267b3fb1c9426006f4e
Paramveer Singh <114360831+paramveer7267@users.noreply.github.com>
2025-04-09 22:20:52 +0530

    Create 3396. Minimum Number of Operations to Make Elements in Array Distinct.cpp

---
+++ Easy/3396. Minimum Number of Operations to Make Elements in Array Distinct.cpp
@@ -0,0 +1,40 @@
+/*
+    LeetCode Easy               : 3396. Minimum Number of Operations to Make Elements in Array Distinct
+    Leetcode Link               : https://leetcode.com/problems/minimum-number-of-operations-to-make-elements-in-array-distinct/description/
+*/
+
+/*********************************************************** C++ **************************************************/
+
+
+
+// Using Brute Force
+// Time Complexity: O(n²)
+// Space COmplexity:O(n)
+class Solution {
+public:
+    bool check(vector<int>& nums,int s) {
+        unordered_set<int> st;
+
+        for (; s < nums.size(); s++) {
+            if (st.count(nums[s])) {
+                return false;
+            }
+            st.insert(nums[s]);
+        }
+        return true;
+    }
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
+        int ops = 0;
+        for (int i = 0; i < n; i += 3) {
+            if (check(nums,i)) {
+                return ops;
+            }
+            ops++;
+        }
+        return ops;
+    }
+};
+
+
+
