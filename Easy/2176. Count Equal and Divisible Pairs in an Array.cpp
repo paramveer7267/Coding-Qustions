704cf10240c5e342c30cdc56911798550eff75a0
Paramveer Singh <114360831+paramveer7267@users.noreply.github.com>
2025-04-19 16:56:38 +0530

    Update 2176. Count Equal and Divisible Pairs in an Array.cpp

--- Easy/2176. Count Equal and Divisible Pairs in an Array.cpp
+++ Easy/2176. Count Equal and Divisible Pairs in an Array.cpp
@@ -1,4 +1,4 @@
-/*
+ /*
     LeetCode Easy               : 2176. Count Equal and Divisible Pairs in an Array
     LeetCode Link               : https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/description/
 */
@@ -22,5 +22,49 @@ public:
             }
         }
         return ans;
+    }
+};
+
+
+
+// Using Sliding Window
+// Time Complexity: O(n * (log(k) + sqrt(k)))
+// Space Complexity: O(n)
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int n = nums.size();
+        int res = 0;
+
+        unordered_map<int,vector<int>> indicesMap;
+        for(int i = 0;i<n ;i++){
+            indicesMap[nums[i]].push_back(i);
+        }
+
+        unordered_set<int> div;
+        for(int f = 1; f * f <= k;f++){
+            if(k % f==0){
+                div.insert(f);
+                div.insert(k/f);
+            }
+        }
+
+        for(auto &[num,indices] : indicesMap){
+            unordered_map<int,int> factorsMap;
+
+            for(int i :indices){
+                int GCD = gcd(i,k);
+                int j = k / GCD;
+
+                res += factorsMap[j];
+
+                for(int f:div){
+                    if(i % f == 0){
+                        factorsMap[f]++;
+                    }
+                }
+            }
+        }
+        return res;
     }
 };
