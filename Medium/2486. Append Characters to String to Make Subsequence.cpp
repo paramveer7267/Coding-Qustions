f1fa9a7c5fe54eadb10597c46304d9bc96581f35
Paramveer Singh <114360831+paramveer7267@users.noreply.github.com>
2025-04-21 12:35:33 +0530

    Update 2486. Append Characters to String to Make Subsequence.cpp

--- Medium/2486. Append Characters to String to Make Subsequence.cpp
+++ Medium/2486. Append Characters to String to Make Subsequence.cpp
@@ -1,1 +1,26 @@
+/*
+    LeetCode Medium             : 2486. Append Characters to String to Make Subsequence
+    LeetCode Link               : https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/description/
+*/
 
+/*********************************************************** C++ **************************************************/
+
+
+// Using Two Pointers
+// Time Comp : O(n)
+// Space Comp: O(1)
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int i = 0;
+        int j = 0;
+        while (i < s.size()) {
+            if (s[i] == t[j]) {
+                j++;
+            }
+            i++;
+        }
+         
+        return t.size() - j;
+    }
+};
